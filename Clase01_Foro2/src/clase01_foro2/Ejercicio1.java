88dac44201cbf185c24b5e7e4f00391c22e06091
Flia Mayorga <Flia Mayorga@DESKTOP-3U4O0UQ>
2020-06-09 19:40:33 -0300

    Actualización Clase 1 Foro 2 de Programación II

--- Clase01_Foro2/src/clase01_foro2/Ejercicio1.java
+++ Clase01_Foro2/src/clase01_foro2/Ejercicio1.java
@@ -7,13 +7,13 @@ public class Ejercicio1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-Scanner scanner=new Scanner (System.in);
+Scanner Leer=new Scanner (System.in);
 		
 		System.out.print ("Ingrese un numero: ");
-		int num1 = scanner.nextInt();
+		int num1 = Leer.nextInt();
 		
 		System.out.print ("Ingrese un numero: ");
-		int num2 = scanner.nextInt();
+		int num2 = Leer.nextInt();
 		
 		int resultado = num1+num2;
 		
