65af2b8f3278ec4dfc1fd7577aa416f311180957
FliaMayorga <FliaMayorga@DESKTOP-NSO1T0D>
2020-07-01 18:14:17 -0300

    Actualización de archivos

---
+++ Clase_02_Foro3/src/vectoresbasico/ControladorBasico.java
@@ -0,0 +1,105 @@
+package vectoresbasico;
+
+import java.util.Scanner;
+
+public class ControladorBasico {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner Leer = new Scanner(System.in);
+		
+		LibretaBasica miLibreta = new LibretaBasica();
+		
+		/*miLibreta.agregarAlibreta("Enzo", 3);
+		miLibreta.listarLibreta();
+		miLibreta.agregarAlibreta("Jerónimo", 5);
+		miLibreta.listarLibreta();
+		miLibreta.agregarAlibreta("Marcela", 8);
+		miLibreta.listarLibreta();
+		miLibreta.agregarAlibreta("Patricia", 1);
+		miLibreta.listarLibreta();
+		miLibreta.agregarAlibreta("Ignacio", 0);
+		miLibreta.listarLibreta();
+		miLibreta.agregarAlibreta("Cristian",6);
+		miLibreta.listarLibreta();
+		miLibreta.agregarAlibreta("Francisco", 2);
+		miLibreta.listarLibreta();
+		miLibreta.agregarAlibreta("Cata", 9);
+		miLibreta.listarLibreta();
+		miLibreta.agregarAlibreta("Luli", 4);
+		miLibreta.listarLibreta();
+		miLibreta.agregarAlibreta("Elisa", 7);
+		miLibreta.listarLibreta();*/
+		
+		int opciones;
+		
+		do {
+			System.out.println("MENU PRINCIPAL");
+			System.out.println("..........................");
+			System.out.println("Elija una opción numérica:");
+			System.out.println("..........................");
+			
+			System.out.println("1. Agregar Nombre y Posición");
+			System.out.println("2. Eliminar un Nombre");
+			System.out.println("3. Eliminar una posición");
+			System.out.println("4. Mostrar la libreta básica");
+			System.out.println("5. Salida");
+			
+			opciones = Leer.nextInt();
+			
+			if (opciones == 1) {
+				
+				System.out.println("Escribir el nombre");
+				String nombrenuevo = Leer.next();
+				
+				System.out.println("Escribir la posición donde guardar el nombre");
+				int posicionnueva = Leer.nextInt();
+				
+				miLibreta.agregarAlibreta(nombrenuevo, posicionnueva);
+				miLibreta.listarLibreta();					
+			}
+			
+			if (opciones == 2) {
+				
+				System.out.println("Escribir el Nombre que quiere eliminar");
+				String nombreaeliminar = Leer.next();
+				
+				miLibreta.nombreAremover(nombreaeliminar);
+				miLibreta.listarLibreta();
+			}
+			
+			if (opciones == 3) {
+				
+				System.out.println("Escribir la posición que quiere eliminar");
+				int posicionaeliminar = Leer.nextInt();
+				
+				miLibreta.posicionAremover(posicionaeliminar);
+				miLibreta.listarLibreta();
+			
+			} 
+			
+			if (opciones == 4) {
+				
+				System.out.println("La lista completa quedará asi");
+				
+				miLibreta.listarLibreta();
+			}
+			
+			if (opciones == 5) {
+				
+				System.out.println("Gracias por utililizar este Menu - Vuelva prontos");
+				break;
+				
+			}
+			
+		} while (opciones != 0);
+				
+		
+			
+	}		
+
+}
+
+
+
---
+++ Clase_02_Foro3/src/vectoresbasico/LibretaBasica.java
@@ -0,0 +1,77 @@
+package vectoresbasico;
+
+import java.util.Arrays;
+
+public class LibretaBasica {
+		
+	String nombres[] = new String[10];
+	
+	public void agregarAlibreta(String nom, int lugar) {
+		
+		this.nombres[lugar] = nom;
+			
+	//creo bucle for para recorrer el array
+			for(int i=0; i<nombres.length; i++) {
+				
+				//si es igual pasa a este if donde comprueba la posicion 
+						if (nombres[i] == null) {
+							
+							//y si es igual no la imprime
+							nombres[i]="";
+						}
+			}
+	}	
+	
+		
+	public void nombreAremover(String nombreaeliminar) {
+		// TODO Auto-generated method stub
+		
+			for(int i=0; i<10; i++) {
+			
+				if(nombres[i].equals(nombreaeliminar)) {
+				
+					nombres[i] = null;
+			}
+		}	
+	
+	}
+	
+	public void posicionAremover(int posicionaeliminar) {
+		// TODO Auto-generated method stub
+		
+		for(int i=0; i<nombres.length; i++) {
+			
+			//este bucle comienza en 0 y lo va a repetir 10 veces
+			for(int j1=0; j1<nombres.length-1; j1++) {
+				
+				//este condicional es para comprobar si i es igual a j o distinto
+				if(j1 == posicionaeliminar) {
+					
+					//y si es igual no la imprime
+						nombres[j1] = "Posición eliminada";
+					
+				}
+			}		
+		
+		}
+	}
+	public void listarLibreta() {
+
+		for(int i=0;i<nombres.length;i++) {
+							
+			System.out.println("Vector " + i + " se ubica en la posición " + (i+1) + "-->" + nombres[i]);
+		}
+	}
+
+
+	
+	}
+
+
+	
+
+	
+
+
+	
+
