0acc0ee5af2c4d8570b657a315318dac2869cda3
FliaMayorga <FliaMayorga@DESKTOP-NSO1T0D>
2020-07-13 18:07:05 -0300

    Actualizaciones Ciudadano y Persona Clase 2 - Foro 4

--- Clase_01_Foro3_4/src/persona/Ciudadano.java
+++ Clase_01_Foro3_4/src/persona/Ciudadano.java
@@ -45,7 +45,7 @@ public class Ciudadano extends Persona {
 		Ciudadano alumno1 = new Ciudadano("Facundo", "Mayorga", "Mendoza", 24460919);
 		Ciudadano alumno2 = new Ciudadano("Armando", "Bardo", "Lavaie", 45678934);
 		Ciudadano alumno3 = new Ciudadano("Esteban", "Quito", "San Luis", 35735918);
-		Ciudadano alumno4 = new Ciudadano("Elena", "Nito", "Cordoooooba", 40256341);	
+		Ciudadano alumno4 = new Ciudadano("Elena", "Nito", "Cordoooooba", 42256341);	
 		Ciudadano alumno5 = new Ciudadano("Lola", "Mento", "Santa Rosa", 44710739);
 		
 		System.out.println(alumno1);
